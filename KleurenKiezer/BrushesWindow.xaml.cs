3b792694b2e02e9796a34cea0e7ac523957f5fc0
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-05-09 15:07:43 +0200

    layout options unfinished

---
+++ KleurenKiezer/BrushesWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace KleurenKiezer
+{
+    /// <summary>
+    /// Interaction logic for BrushesWindow.xaml
+    /// </summary>
+    public partial class BrushesWindow : Window
+    {
+        public BrushesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
