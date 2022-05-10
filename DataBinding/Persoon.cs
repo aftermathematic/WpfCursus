b11035b0dba17d1da7db30e2f50ed1e1732ab486
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-05-10 19:25:23 +0200

    Databinding

---
+++ DataBinding/Persoon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace DataBinding;
+
+public class Persoon : ObservableObject
+{
+    private string naamValue;
+    private decimal weddeValue;
+    private DateTime inDienstValue;
+
+    public Persoon(string nNaam, decimal nWedde, DateTime nInDienst)
+    {
+        Naam = nNaam;
+        Wedde = nWedde;
+        InDienst = nInDienst;
+    }
+
+    public string Naam
+    {
+        get => naamValue;
+        set => SetProperty(ref naamValue, value);
+    }
+
+    public decimal Wedde
+    {
+        get => weddeValue;
+        set => SetProperty(ref weddeValue, value);
+    }
+
+    public DateTime InDienst
+    {
+        get => inDienstValue;
+        set => SetProperty(ref inDienstValue, value);
+    }
+}
