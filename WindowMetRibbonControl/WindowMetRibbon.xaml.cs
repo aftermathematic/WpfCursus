55a6c4ddf42aae3f96a7ddfd9b01945d196352b5
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-05-11 16:40:01 +0200

    Ribbon converters

--- WindowMetRibbonControl/WindowMetRibbon.xaml.cs
+++ WindowMetRibbonControl/WindowMetRibbon.xaml.cs
@@ -124,4 +124,41 @@ namespace WindowMetRibbonControl
             TextBoxVoorbeeld.Foreground = kleur;
         }
     }
+
+
+
+    public class BooleanToFontWeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            if ((Boolean)value)
+                return "Bold";
+            else
+                return "Normal";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class BooleanToFontStyle : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            if ((Boolean)value)
+                return "Italic";
+            else
+                return "Normal";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
 }
