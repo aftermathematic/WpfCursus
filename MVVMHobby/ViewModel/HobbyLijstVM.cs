6c4980c3ec2238a2118222a53422eceb7e05dd0d
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-05-16 15:04:27 +0200

    MVVM Hobby

---
+++ MVVMHobby/ViewModel/HobbyLijstVM.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using MVVMHobby.View;
+
+namespace MVVMHobby.ViewModel;
+
+public class HobbyLijstVM : ObservableObject
+{
+    private ObservableCollection<HobbyVM> hobbyLijst = new();
+    private HobbyVM selectedHobby;
+    private ImageView groteView;
+
+    public HobbyLijstVM()
+    {
+        HobbyLijst.Add(new HobbyVM("sport", "voetbal",
+            new BitmapImage(new Uri("pack://application:,,,/Images/voetbal.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("sport", "atletiek",
+            new BitmapImage(new Uri("pack://application:,,,/Images/atletiek.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("sport", "basketbal",
+            new BitmapImage(new Uri("pack://application:,,,/Images/basketbal.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("sport", "tennis",
+            new BitmapImage(new Uri("pack://application:,,,/Images/tennis.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("sport", "turnen",
+            new BitmapImage(new Uri("pack://application:,,,/Images/turnen.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("muziek", "trompet",
+            new BitmapImage(new Uri("pack://application:,,,/Images/trompet.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("muziek", "drum",
+            new BitmapImage(new Uri("pack://application:,,,/Images/drum.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("muziek", "gitaar",
+            new BitmapImage(new Uri("pack://application:,,,/Images/gitaar.jpg",
+                UriKind.Absolute))));
+        HobbyLijst.Add(new HobbyVM("muziek", "piano",
+            new BitmapImage(new Uri("pack://application:,,,/Images/piano.jpg",
+                UriKind.Absolute))));
+
+        VerwijderCommand = new RelayCommand(Verwijder);
+
+        MouseDownEvent = new RelayCommand<MouseEventArgs>(x => MouseDown(x));
+        MouseUpEvent = new RelayCommand<MouseEventArgs>(x => MouseUp(x));
+    }
+
+    public ObservableCollection<HobbyVM> HobbyLijst
+    {
+        get => hobbyLijst;
+        set => SetProperty(ref hobbyLijst, value);
+    }
+
+    public HobbyVM SelectedHobby
+    {
+        get => selectedHobby;
+        set => SetProperty(ref selectedHobby, value);
+    }
+
+    public ICommand VerwijderCommand { get; }
+    public ICommand MouseDownEvent { get; }
+    public ICommand MouseUpEvent { get; }
+
+    private void MouseDown(MouseEventArgs e)
+    {
+        var tg = (Image) e.OriginalSource;
+        groteView = new ImageView();
+        groteView.GroteImage.Source = tg.Source;
+        groteView.Show();
+    }
+
+    private void MouseUp(MouseEventArgs e)
+    {
+        if (groteView != null)
+        {
+            groteView.Close();
+        }
+
+        groteView = null;
+    }
+
+    private void Verwijder()
+    {
+        HobbyLijst.Remove(SelectedHobby);
+    }
+}
---
+++ MVVMHobby/ViewModel/HobbyVM.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Media.Imaging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace MVVMHobby.ViewModel;
+
+public class HobbyVM : ObservableObject
+{
+    public HobbyVM(string nCategorie, string nActiviteit, BitmapImage nSymbool)
+    {
+        Categorie = nCategorie;
+        Activiteit = nActiviteit;
+        Symbool = nSymbool;
+    }
+    private string categorieValue;
+    private string activiteitValue;
+    private BitmapImage symboolValue;
+    public string Categorie
+    {
+        get { return categorieValue; }
+        set => SetProperty(ref categorieValue, value);
+    }
+    public string Activiteit
+    {
+        get { return activiteitValue; }
+        set => SetProperty(ref activiteitValue, value);
+    }
+    public BitmapImage Symbool
+    {
+        get { return symboolValue; }
+        set => SetProperty(ref symboolValue, value);
+    }
+}
